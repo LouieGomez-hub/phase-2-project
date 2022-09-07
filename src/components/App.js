b7576a46edb0ed901d04f976bfa991e2736f5e7f
louie_gomez <kinggomez027.lg@gmail.com>
2022-09-07 18:41:45 -0400

    App component imports/export

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import HeroShowcase from "./HeroShowcase";
+import HeroStats from "./HeroStats";
+
+export default App;
