1766c96703be4a57b7a4a394e5e7125745dee2a7
louie_gomez <kinggomez027.lg@gmail.com>
2022-09-07 20:05:26 -0400

    App component functionality

--- src/components/App.js
+++ src/components/App.js
@@ -5,26 +5,23 @@ import Home from "./Home";
 import HeroShowcase from "./HeroShowcase";
 import HeroStats from "./HeroStats";
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function App() {
+  return (
+    <div>
+       <NavBar />
+       <Switch>
+          <Route exact path="/heroshowcase">
+            <HeroShowcase />
+          </Route>
+          <Route exact path="/herostats">
+            <HeroStats />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+       </Switch>
+    </div>
+  );
+}
 
 export default App;
