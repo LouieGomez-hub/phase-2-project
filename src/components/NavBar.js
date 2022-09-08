b75e406c68abd7f638ff228aa3c2c6688eefc09f
louie_gomez <kinggomez027.lg@gmail.com>
2022-09-08 19:06:06 -0400

    NavLink imported and added to function

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function NavBar() {
-
+  return (
+    <div className="navbar">
+      <NavLink
+        to="/"
+        exact
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/heroshowcase"
+        exact
+      >
+        Showcase
+      </NavLink>
+      <NavLink
+        to="/herostats"
+        exact
+      >
+        Stats
+      </NavLink>
+    </div>
+  );
 }
 
 
