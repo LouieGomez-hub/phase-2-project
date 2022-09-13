d7ee7d05da65c4ddecdbf8affc7ad835251ceb92
louie_gomez <kinggomez027.lg@gmail.com>
2022-09-13 18:12:06 -0400

    img added and css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,8 +4,8 @@ import { NavLink } from "react-router-dom";
 function NavBar() {
   return (
     <div className="navbar">
-
-      <img className="super" src="" alt="SuperHero Database"/>
+      
+      <img className="super" src="https://img.favpng.com/10/22/6/superhero-png-favpng-9yKc3QvuS0C0akjqeGUDZMS3U_t.jpg" alt="SuperHero Database"/>
 
       <NavLink
         to="/"
