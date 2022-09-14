57851da9f42050cc14ffe48b51c9ccbfccd14c90
louie_gomez <kinggomez027.lg@gmail.com>
2022-09-14 14:08:03 -0400

    added images to browser to act as buttons

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,13 +6,13 @@ function Home() {
       <h1>Welcome!</h1>
       <p>Read up on some facts about some of the most popular heroes in two universes:</p>
       <button>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Marvel_Logo.svg/1200px-Marvel_Logo.svg.png?20161025051221" alt="Marvel"/>
+        <img className="marvelpng" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Marvel_Logo.svg/1200px-Marvel_Logo.svg.png?20161025051221" alt="Marvel"/>
       </button>
       <button>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/DC_Comics_logo.png/768px-DC_Comics_logo.png" alt="DC"/>
+        <img className="dcpng" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/DC_Comics_logo.png/768px-DC_Comics_logo.png" alt="DC"/>
       </button>
     </div>
-  )
+  );
 }
 
 export default Home;
