22dc67e3226b22f1838ac668f526d43cdf9b79e2
louie_gomez <kinggomez027.lg@gmail.com>
2022-09-14 12:53:42 -0400

    added images as buttons

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,10 +6,10 @@ function Home() {
       <h1>Welcome!</h1>
       <p>Read up on some facts about some of the most popular heroes in two universes:</p>
       <button>
-        <img src="" alt="Marvel"/>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Marvel_Logo.svg/1200px-Marvel_Logo.svg.png?20161025051221" alt="Marvel"/>
       </button>
       <button>
-        <img src="" alt="DC"/>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/DC_Comics_logo.png/768px-DC_Comics_logo.png" alt="DC"/>
       </button>
     </div>
   )
