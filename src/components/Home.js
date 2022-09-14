5ed1c199412be789ce30f29404255172330b7979
louie_gomez <kinggomez027.lg@gmail.com>
2022-09-14 12:50:29 -0400

    text added to Home Page with styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 function Home() {
-  return <h1>Home!</h1>;
+  return (
+    <div className="home">
+      <h1>Welcome!</h1>
+      <p>Read up on some facts about some of the most popular heroes in two universes:</p>
+      <button>
+        <img src="" alt="Marvel"/>
+      </button>
+      <button>
+        <img src="" alt="DC"/>
+      </button>
+    </div>
+  )
 }
 
 export default Home;
