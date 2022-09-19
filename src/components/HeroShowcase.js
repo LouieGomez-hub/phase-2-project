48f7578ef07f6670a346171874a0e8b9eb1c33bb
louie_gomez <kinggomez027.lg@gmail.com>
2022-09-19 15:47:23 -0400

    fetching data from server

--- src/components/HeroShowcase.js
+++ src/components/HeroShowcase.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function HeroShowcase() {
+  const [heroes, setHeroes] = useState('')
+  
+  useEffect(() => {
+    fetch('http://localhost:3000/heroes')
+    .then((res) => res.json())
+    .then((heroes) => 
+      setHeroes(heroes)
+    )
+  }, [])
+
+console.log(heroes)
+
   return (
     <h1>Showcase</h1>
   )
