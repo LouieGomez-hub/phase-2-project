870b1af794716e44da3ba09804a72855d8fa2b71
louie_gomez <kinggomez027.lg@gmail.com>
2022-09-19 16:30:31 -0400

    iterating through fetched data to display in browser

--- src/components/HeroShowcase.js
+++ src/components/HeroShowcase.js
@@ -1,9 +1,22 @@
 import React from "react";
 
 function HeroShowcase({ heroes }) {
+  let display;
+  console.log(heroes);
+
+  display = heroes.map(hero => {
+    let { id, name, image } = hero
+    return (
+      <div key={id} className="col-4">
+        <div className="">
+          <img className="" src={image} alt="" />
+        </div>
+      </div>
+    )
+  });
 
   return (
-    <h1>Showcase</h1>
+    <div>{display}</div>
   )
 }
 
