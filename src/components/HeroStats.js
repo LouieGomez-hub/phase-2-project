51c5a80cac3e0166adfd401060af1f2c1542d254
louie_gomez <kinggomez027.lg@gmail.com>
2022-09-22 21:14:31 -0400

    table of details displayed for each hero

--- src/components/HeroStats.js
+++ src/components/HeroStats.js
@@ -3,22 +3,32 @@ import React from "react";
 function HeroStats({ heroes }) {
   return (
     <div className="stats-table">
-     <table>
+     <table className="table">
        <tr className="table-header">
          <th>Name</th>
-         <th>Primary Skill</th>
          <th>Gender</th>
+         <th>Primary Skill</th>
+         <th>First Appearance</th>
+         <th>Height/Weight</th>
        </tr>
         {heroes.map(hero => {
-        let { name, primarySkill, gender } = hero
+        let { name, 
+              primarySkill, 
+              gender, 
+              firstAppearance, 
+              height, 
+              weight } = hero
+
       return (
         <tr className="table-content">
           <td>{name}</td>
-          <td>{primarySkill}</td>
           <td>{gender}</td>
+          <td>{primarySkill}</td>
+          <td>{firstAppearance}</td>
+          <td>{height} <hr></hr> {weight}</td>
         </tr>
       )
-    })};
+    })}
       </table>
     </div>
   )
