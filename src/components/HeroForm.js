d71ff8bd6e8a2cb5269ca5edeb54fc4576a36fd9
louie_gomez <kinggomez027.lg@gmail.com>
2022-09-30 22:04:14 -0400

    created form to add new heroes

---
+++ src/components/HeroForm.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+
+function HeroForm({onAddHero}) {
+  const [name, setName] = useState("");
+  const [race, setRace] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const heroData = {
+        name: name
+    }
+    fetch('http://localhost:3000/heroes', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(heroData),
+    })
+    .then((res) => res.json())
+    .then((newHero) => onAddHero(newHero));
+  }
+
+  return (
+    <form className="NewHero" onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input
+           type="text"
+           name="name"
+           value={name}
+           onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+
+      <label>
+        Race:
+        <input
+           type="text"
+           name="name"
+           value={race}
+           onChange={(e) => setRace(e.target.value)}
+        />
+      </label>
+
+      <button type="submit">Add New Hero</button>
+    </form>
+  );
+}
+
+export default HeroForm;
