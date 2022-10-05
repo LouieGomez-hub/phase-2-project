6fc05b227214f47640b70dcfa4e8a672c88d11f1
louie_gomez <kinggomez027.lg@gmail.com>
2022-10-05 16:32:01 -0400

    changed home page images from buttons to just images

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,12 +5,12 @@ function Home() {
     <div className="home">
       <h1>Welcome!</h1>
       <p>Read up on some facts about some of the most popular heroes in two universes:</p>
-      <button>
-        <img className="marvelpng" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Marvel_Logo.svg/1200px-Marvel_Logo.svg.png?20161025051221" alt="Marvel"/>
-      </button>
-      <button>
-        <img className="dcpng" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/DC_Comics_logo.png/768px-DC_Comics_logo.png" alt="DC"/>
-      </button>
+        <img className="marvelpng" 
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Marvel_Logo.svg/1200px-Marvel_Logo.svg.png?20161025051221" alt="Marvel"
+        />
+        <img className="dcpng" 
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/DC_Comics_logo.png/768px-DC_Comics_logo.png" alt="DC"
+        />
     </div>
   );
 }
